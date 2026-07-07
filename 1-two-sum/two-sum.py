42689232820fcf3ec0fc4229ecc6911b552a792e
Yasir Y. <yyproton168@icloud.com>
2026-07-06 22:42:20 -0400

    Time: 3974 ms (5.00%) | Memory: 19.7 MB (94.84%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -3,12 +3,13 @@ class Solution:
         pointer1 = 0
         pointer2 = 1
 
-        while(not pointer1 == len(nums)):
+        while not pointer1 == len(nums):
             if(nums[pointer1] + nums[pointer2] == target):
-                return [pointer1, pointer2]
-            else: 
+                return [pointer1 , pointer2]
+            else:
                 pointer2 += 1
             if(pointer2 == len(nums)):
                 pointer1 += 1
                 pointer2 = pointer1 + 1
+        return [-1]
 
